8e5fcbbfb8be64eaebe3874fc548374df9bf610e
Ramon <ramonsilva12305@outlook.com>
2025-08-05 21:16:14 -0300

    feat: Swagger impl

--- book-api/src/main/java/com/f1rst/bookapi/controllers/BookController.java
+++ book-api/src/main/java/com/f1rst/bookapi/controllers/BookController.java
@@ -6,6 +6,8 @@ import com.f1rst.bookapi.dtos.OpenLibraryByKeyResponseDTO;
 import com.f1rst.bookapi.models.BookRecentlyViewModel;
 import com.f1rst.bookapi.services.BookRecentlyViewService;
 import com.f1rst.bookapi.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@Tag(name = "Book", description = "Seaches books from OpenLibrary and manages recently viewed books by users")
 public class BookController {
 
     final BookService bookService;
@@ -25,6 +28,10 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Search general books.",
+            description = "Return a list of books from OpenLibrary."
+    )
     public ResponseEntity<List<BookDTO>> getGeneralBooks(@RequestParam String q,
                                                          @RequestParam(defaultValue = "1") int page,
                                                          @RequestParam(defaultValue = "10") int limit,
@@ -35,6 +42,10 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Find book by Id.",
+            description = "Return book by Book key from OpenLibrary."
+    )
     public ResponseEntity<OpenLibraryByKeyResponseDTO> getBookById(@PathVariable(value = "id") String id,
                                                                    @RequestHeader("userId") String userId) {
             return ResponseEntity.
@@ -43,6 +54,10 @@ public class BookController {
 
     }
 
+    @Operation(
+            summary = "Find book by author.",
+            description = "Return book by Author from OpenLibrary."
+    )
     @GetMapping("/author")
     public ResponseEntity<List<BookDTO>> getBooksByAuthor(@RequestParam String author,
                                                           @RequestParam(defaultValue = "1") int page,
@@ -53,6 +68,10 @@ public class BookController {
                 body(bookService.getAllBooksByAuthor(author, page, limit, userId));
     }
 
+    @Operation(
+            summary = "Find book by gender.",
+            description = "Return book by Gender from OpenLibrary."
+    )
     @GetMapping("/gender")
     public ResponseEntity<List<BookByGenderDTO>> getBooksByGender(@RequestParam(value = "gender") String gender,
                                                                   @RequestParam(defaultValue = "1") int page,
@@ -63,6 +82,11 @@ public class BookController {
                 body(bookService.gelAllBooksByGender(gender, page, limit, userId));
     }
 
+
+    @Operation(
+            summary = "Get recently viewed books by user.",
+            description = "Return a list of recently viewed books by user."
+    )
     @GetMapping("/users/{userId}/recently-viewed")
     public ResponseEntity<List<BookRecentlyViewModel>> getRecentBooks(@PathVariable String userId) {
         return ResponseEntity.ok(bookRecentlyViewService.getRecentlyViewedBooks(userId));
